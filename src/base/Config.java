061425b8a1d2412fdfe01a4b117d001dedb0e346
MMKRahul <48232594+MMKRahul@users.noreply.github.com>
2019-05-24 00:19:11 -0500

    commit

--- src/base/Config.java
+++ src/base/Config.java
@@ -1,7 +1,38 @@
 package base;
 
-public class Config {
-	
-	
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+import utils.WebDriverFunctions;
+
+public class Config extends WebDriverFunctions {
+	
+	@BeforeTest
+	public void BeforeTestStart() {
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\DevilR\\Documents\\GitHub\\Carguru\\drivers\\chromedriver.exe");
+		// init the chrome driver
+		driver = new ChromeDriver();
+		
+		driver.get("https://www.cargurus.com/");
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+	
+	@AfterMethod
+	public void afterEachTest(){
+		driver.close();
+	}
+
+	//@AfterSuite
+	//public void suiteComplete(){
+		//driver.quit();
+		//currentTime();
+	
 }
---
+++ src/locators/CarguruLocators.java
@@ -0,0 +1,7 @@
+package locators;
+
+public class CarguruLocators {
+	
+	public String Usedcar = "//*[@id=\'cg-navbar\']/ul[1]/li[1]/a/span";
+
+}
